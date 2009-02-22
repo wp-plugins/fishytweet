677c1c8efabc0b707b93f22b76d75913a2f997d6
fiskeben <fiskeben@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 14:32:57 +0000

    Fixed version number
    git-svn-id: https://plugins.svn.wordpress.org/fishytweet/trunk@96643 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fishytweet.php
+++ fishytweet.php
@@ -3,7 +3,7 @@
 Plugin Name: Fishy Tweet
 Plugin URI: http://fiskeben.dk/fishytweet
 Description: Adds "Tweet this" to posts.
-Version: 1.6.1
+Version: 1.6.2
 Author: Ricco Førgaard
 Author URI: http://fiskeben.dk
 */
